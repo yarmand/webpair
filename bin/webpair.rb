752cd6d4203df9f953aea367621ca6502b1d8ac2
yann ARMAND <yarmand@yammer-inc.com>
2013-04-07 20:29:33 +0100

    1st implement of auto install on reomte

--- bin/webpair.rb
+++ bin/webpair.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env bundle exec ruby
 
 require 'net/ssh'
- require 'yaml'
+require 'yaml'
 
+def log(str)
+  puts str
+end
 
 def open_connection(args = {})
   server = args['server'] || 'NO SERVER'
@@ -11,23 +14,38 @@ def open_connection(args = {})
   port = 8100 + Random.rand(100)
   pguest = args['local_guest_user'] || 'NO_USER'
   key = random_string
-  puts "port: #{port}"
-  puts "random_string: #{key}"
-  puts "server: #{server}"
-  puts "user: #{user}"
-  puts "URL:\nhttp://#{server}:#{remote_port}/webpair?key=#{key}"
+  log "port: #{port}"
+  log "random_string: #{key}"
+  log "server: #{server}"
+  log "user: #{user}"
+  puts ">>> remote webpair URL:\n\nhttp://#{server}:#{remote_port}/webpair?key=#{key}\n"
   Net::SSH.start(server, user, password: 'od82prn3') do |ssh|
     ssh.forward.remote(22, "localhost", port)
+    #installed = ssh.exec "test -d webpair && echo 'OK'"
+    #remote_install(ssh) unless installed == 'OK'
     ssh.exec "cd webpair ; echo '#{key}=#{port}:#{pguest}' >webpair.keys"
     ssh.exec "cd webpair ; node lib/app.js"
   end
+end
 
+def remote_install(ssh)
+  log '### Installing on remote host'
+  ssh.exec "cd"
+  repo = `git remote -v | grep fetch | cut -f 2`.sub(%r{.*@github.com:},'http://www.github.com/').sub(%r{\.git\b.*\n},'')
+  log "cloning webpair repository: #{repo}"
+  ssh.exec "git clone #{repo}"
+  ssh.exec "git checkout webpair"
+  log 'installing node modules'
+  ssh.exec 'cd webpair/tty.js'
+  ssh.exec 'npm install'
+  log '### Done'
+  ssh.exec "cd"
 end
 
 def random_string
   (0...20).map{ ('a'..'z').to_a[rand(26)] }.join
 end
 
-config = YAML.load_file('config.yml')
+config = YAML.load_file('../config.yml')
 open_connection(config)
 
